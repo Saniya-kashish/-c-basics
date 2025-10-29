28b7e006f9d24863664f8f7d973cffa1dbcf76b7
Saniya Kashish <saniyakashish20@gmail.com>
2025-10-29 16:38:29 +0530

    Add files via upload

---
+++ c_program3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{ int a,b;
+printf("enter the first number:\n");
+scanf("%d",&a);
+printf("enter the second number:\n");
+scanf("%d",&b);
+printf("the sum is:%d",a+b);
+return 0;
+}
