a3f096c0846cf8e74d297cea5998f0b3627e9448
Saniya Kashish <saniyakashish20@gmail.com>
2025-10-29 18:26:11 +0530

    Add files via upload

---
+++ day1.1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{ int a; 
+  char b;
+  printf("enter the number:\n");
+  scanf("%d",&a);
+  printf("enter the character:\n");
+  scanf(" %c",&b);
+  printf("the number:%d\n",a);
+  printf("the character: %c\n",b);
+  return 0;
+
+}
